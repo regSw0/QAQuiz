97c6ae85b4bea041d42a6a70007369880c1aea33
regSw0 <regina.sw06@gmail.com>
2023-06-07 20:27:07 +0400

    Initial Commit

---
+++ QAQuiz/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QAQuiz
+//
+//  Created by User on 07.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QAQuiz/QAQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QAQuizApp.swift
+//  QAQuiz
+//
+//  Created by User on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct QAQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
