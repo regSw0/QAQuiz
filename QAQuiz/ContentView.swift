4c93f97f5e5e1cbad9911fddf098a91a75afbe25
regSw0 <regina.sw06@gmail.com>
2023-06-14 20:09:30 +0400

    save

--- QAQuiz/ContentView.swift
+++ QAQuiz/ContentView.swift
@@ -10,12 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+            Text("QA")
+                .font(.custom("Anton-Regular", size: 70))
+            Text("Quiz".uppercased())
+                .tracking(15)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.red).opacity(0.5)
+        .background(Image("BG").resizable()
+            .ignoresSafeArea()
+        )
+        
     }
 }
 
