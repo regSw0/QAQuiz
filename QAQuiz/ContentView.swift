d148ceb7d7bbc52c2be1e31f41e59e21dc08ff2a
regSw0 <regina.sw06@gmail.com>
2023-06-28 20:31:55 +0400

    add readmi

--- QAQuiz/ContentView.swift
+++ QAQuiz/ContentView.swift
@@ -6,13 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct ContentView: View {
+    @State var index = 0
+    @FirestoreQuery(collectionPath: "questions") var questions: [Question]
+    @State private var showingSheet = false
     var body: some View {
         VStack {
-
+            ForEach(questions) { question in
+                Text(question.title)
+            }
             Text("QA")
                 .font(.custom("Anton-Regular", size: 70))
+                .onTapGesture {
+                    showingSheet.toggle()
+                }
             Text("Quiz".uppercased())
                 .tracking(15)
         }
@@ -22,7 +33,18 @@ struct ContentView: View {
         .background(Image("BG").resizable()
             .ignoresSafeArea()
         )
-        
+        .actionSheet(isPresented: $showingSheet) {
+            ActionSheet(title: Text(questions[index].title),
+                        message: Text("Pick one"),
+                        buttons: [ .default(Text(questions[index].answers[0]))
+                                   , .default(Text(questions[index].answers[1]), action: add)
+                            , .cancel()
+                        ])
+        }
+    }
+    func add() {
+        index += 1
+        print(index)
     }
 }
 
@@ -30,4 +52,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Question: Identifiable, Codable {
+    @DocumentID var id: String?
+    let title: String
+    let answers: [String]
+    let rightAnswer: String
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+#  LSTQB exam preparation
+
+[Design](https://www.figma.com/file/otyD9YW3Kz1nJhvX6w3rVZ/Einsteen-(Community)?type=design&node-id=0%3A1&mode=design&t=I7ukmXeClKN2E2uL-1)
